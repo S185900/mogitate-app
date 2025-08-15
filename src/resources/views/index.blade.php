28121e27598de887be55d6cef681db8503fb5760
S <serena.sa.em.na.ra@gmail.com>
2025-08-15 16:10:47 +0900

    タブ消えてresetまで実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -35,13 +35,13 @@
 
                 <!-- ソート検索されたら表示されるタブ -->
                 <div class="index__inner__sidebar-filter-tabs" style="{{ request('sort') == 'high' ? 'display: block;' : 'display: none;' }}">
-                        <label class="index__inner__sidebar-filter-tabs__display" for="close">
+                        <label class="index__inner__sidebar-filter-tabs__display" for="close" onclick="window.location.href='/products/search'">
                             <input class="close-tab" type="checkbox" value="" id="close" />
                             高い順に表示<span class="close_btn"></span>
                         </label>
                 </div>
                 <div class="index__inner__sidebar-filter-tabs" style="{{ request('sort') == 'low' ? 'display: block;' : 'display: none;' }}">
-                    <label class="index__inner__sidebar-filter-tabs__display" for="close">
+                    <label class="index__inner__sidebar-filter-tabs__display" for="close" onclick="window.location.href='/products/search'">
                         <input class="close-tab" type="checkbox" value="" id="close" />
                         低い順に表示<span class="close_btn"></span>
                     </label>
@@ -67,4 +67,3 @@
     </div>
 </div>
 @endsection
-
